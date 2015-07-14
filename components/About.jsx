c3f67e9407cd8ebc2b52f72da04f8b3b15036d39
Pete Petrash <pete.petrash@gmail.com>
2015-07-14 16:32:28 -0700

    more setup

---
+++ components/About.jsx
@@ -0,0 +1,14 @@
+
+var React = require('react')
+
+var About = React.createClass({
+  render: function () {
+    return (
+      <main>
+        About component
+      </main>
+    )
+  }
+})
+
+module.exports = About
---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+var Router = require('react-router')
+var Link = Router.Link
+
+var Header = React.createClass({
+  render: function () {
+    return (
+      <header>
+        <Link to='/'>Index</Link>
+        <Link to='/about'>About</Link>
+      </header>
+    )
+  }
+})
+
+module.exports = Header
